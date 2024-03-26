d496bf31fc208adca57d25767b2d9adf6197a7fb
Sergey <kolesnik.s@future-group.ru>
2024-03-26 11:18:42 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+
+// const url = "https://mellow-florentine-74b2ce.netlify.app";
 
 function App() {
+  const handleFullscreen = () => {
+    document.documentElement.requestFullscreen();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TG HTML5 GAME</h1>
+      <button onClick={handleFullscreen}>Fullscreen</button>
     </div>
   );
 }
